62dcd7cba43273c7947977df1e84232e01ba315f
Chris Hunt <chunt@climate.com>
2012-01-16 17:58:53 -0800

    Begin implementation of get_lat_lon, not complete

--- spec/lib/interior_spec.rb
+++ spec/lib/interior_spec.rb
@@ -1,4 +1,48 @@
 require 'spec_helper.rb'
 
-describe Interior do
+describe Interior::Geocoder do
+  before(:all) do
+    @coder = Interior::Geocoder
+  end
+
+  describe '#get_lat_lon' do
+    it 'returns a hash' do
+      @coder.get_lat_lon(nil, nil, nil, nil).class.should == Hash
+    end
+
+    it 'returns a collection of size 2' do
+      @coder.get_lat_lon(nil, nil, nil, nil).size.should == 2
+    end
+
+    it 'returns collection with key :lat' do
+      @coder.get_lat_lon(nil, nil, nil, nil).keys.include?(:lat).should == true
+    end
+
+    it 'returns collection with key :lon' do
+      @coder.get_lat_lon(nil, nil, nil, nil).keys.include?(:lon).should == true
+    end
+
+    it 'returns float values' do
+      result = @coder.get_lat_lon(nil, nil, nil, nil)
+      result.values.each { |v| v.class.should == Float }
+    end
+
+    it 'returns latitude between -90 and 90' do
+      result = @coder.get_lat_lon(14, '1N', '1E', 35)
+      (result[:lat] >= -90).should == true
+      (result[:lat] <=  90).should == true
+    end
+
+    it 'returns longitude between -180 and 180' do
+      result = @coder.get_lat_lon(14, '1N', '1E', 35)
+      (result[:lon] >= -180).should == true
+      (result[:lon] <=  180).should == true
+    end
+
+    it 'correctly maps meridian, township, range, section to lat, lon' do
+      @coder.get_lat_lon(14, '1N', '1E', 35).should == {
+        :lat => 33.384549272498, :lon => -112.228362739723
+      }
+    end
+  end
 end
