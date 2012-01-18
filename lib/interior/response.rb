402b01fb288621cededbabd7121b1c92e129497f
Chris Hunt <chunt@climate.com>
2012-01-18 10:59:17 -0800

    Add success? to Interior::Response

--- lib/interior/response.rb
+++ lib/interior/response.rb
@@ -2,9 +2,14 @@ module Interior
   class Response
     attr_reader :latitude, :longitude
 
-    def initialize(latitude, longitude)
+    def initialize(latitude = nil, longitude = nil, success = false)
       @latitude  = latitude
       @longitude = longitude
+      @success   = success
+    end
+
+    def success?
+      @success
     end
   end
 end
--- spec/lib/interior/response_spec.rb
+++ spec/lib/interior/response_spec.rb
@@ -4,7 +4,23 @@ require 'interior/response.rb'
 describe Interior::Response do
   let(:latitude)  { 30 }
   let(:longitude) { 60 }
-  let(:response)  { Interior::Response.new(latitude, longitude) }
+  let(:success)   { true }
+  let(:response)  { Interior::Response.new(latitude, longitude, success) }
+
+  describe('#success?') do
+    it 'defaults to false' do
+      Interior::Response.new.success?.should == false
+    end
+
+    it 'can be initialized' do
+      response.success?.should == true
+    end
+
+    it 'is readonly' do
+      lambda { response.success = false }.should raise_error
+    end
+
+  end
 
   describe('#latitude') do
     it 'can be initialized' do
